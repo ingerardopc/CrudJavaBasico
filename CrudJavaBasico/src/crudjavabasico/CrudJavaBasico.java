cb00fd0b93f1608c2f3166fc1198f8fb0cc85ffb
INGERARDOPC <78515434+ingerardopc@users.noreply.github.com>
2022-08-27 18:52:03 -0500

    Update CrudJavaBasico.java
    probando haciendo cambios desde consola

--- CrudJavaBasico/src/crudjavabasico/CrudJavaBasico.java
+++ CrudJavaBasico/src/crudjavabasico/CrudJavaBasico.java
@@ -10,6 +10,7 @@ public class CrudJavaBasico {
     
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("cambios desde consola");
         
     }
     
